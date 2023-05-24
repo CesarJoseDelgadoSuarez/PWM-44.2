70e54c45094fb87dd13ab8d95b4ba56527ef2be4
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2023-05-24 13:17:08 +0100

    add pagination and style in page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
 import { FirestoreService } from '../../services/firestore.service';
 import { log } from 'console';
 import { Observable } from 'rxjs';
 import { UserModel } from '../../user/user.model';
-import {GasStation} from "../../models/GasStation/gas-station.model";
-import {GasStationService} from "../../services/gas-station.service";
+import { Component, OnInit } from '@angular/core';
+import { GasStationService } from '../../services/gas-station.service';
+import { GasStation } from '../../models/GasStation/gas-station.model';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,7 +13,9 @@ import {GasStationService} from "../../services/gas-station.service";
 })
 export class HomePage implements OnInit {
   public gasStations: GasStation[] = [];
-  public favoriteGasStations: GasStation[] = [];
+  public displayedGasStations: GasStation[] = [];
+  public pageSize = 10;
+  public currentPage = 1;
 
   constructor(private gasStationService: GasStationService) {}
 
@@ -21,13 +24,25 @@ export class HomePage implements OnInit {
   }
 
   getGasStations() {
-    this.gasStationService
-      .getCanaryIslandsGasStations()
-      .subscribe((gasStations) => {
-        this.gasStations = gasStations;
-      });
+    this.gasStationService.getCanaryIslandsGasStations().subscribe((gasStations) => {
+      this.gasStations = gasStations;
+      this.loadMore();
+    });
   }
-  /*
+
+  loadMore() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    const newItems = this.gasStations.slice(startIndex, endIndex);
+    this.displayedGasStations = this.displayedGasStations.concat(newItems);
+    this.currentPage++;
+  }
+}
+
+
+
+
+/*
   getFavoriteGasStations() {
     this.gasStationService.getFavoriteGasStations().subscribe((gasStations) => {
       this.favoriteGasStations = gasStations;
@@ -35,4 +50,3 @@ export class HomePage implements OnInit {
     });
   }
   */
-}
